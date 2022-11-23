7e162a2e54b298ab6d550196b0cf30858eaf544b
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-23 10:48:21 +0530

    Добавьте файлы проекта.

---
+++ ListExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            List<int> scores = new List<int>();
+
+            while (count < numOfPlayers)
+            {
+                int score = Convert.ToInt32(Console.ReadLine());
+                scores.Add(score);
+                count++;
+            }
+
+            scores.Sort();
+
+            foreach (int result in scores)
+            {
+                Console.Write(result + " ");
+            }
+        }
+    }
+}
